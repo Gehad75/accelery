bc0bd129f0b887f0e7a0ca81a6baaa25907fad07
Gehad Salem <gehad.20334@stemalex.moe.edu.eg>
2020-12-05 00:40:18 +0200

    edit app name

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Capstone Project'),
+    'name' => env('APP_NAME', 'Accelery'),
 
     /*
     |--------------------------------------------------------------------------
